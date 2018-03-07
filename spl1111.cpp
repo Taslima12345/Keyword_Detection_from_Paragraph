2b48da3e08d9f5717f7e4c33b4ac1e7a83b41204
Taslima12345 <35329197+Taslima12345@users.noreply.github.com>
2018-03-07 09:43:15 +0600

    Add files via upload

---
+++ spl1111.cpp
@@ -0,0 +1,214 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<sstream>
+
+using namespace  std ;
+char **arr;
+string str;
+string word[10000],wrd[10000];
+int num[10000];
+int len=0;
+
+
+void Sort(string *wrd,int *num ,int x)
+{
+    int tmp1;   string tmp2;
+    for(int i=0; i<x-1; i++)
+        {
+        for(int j=i+1; j<x; j++)
+        {
+            if(num[j]>num[i])
+            {
+                tmp1= num[i];   tmp2=wrd[i];
+                num[i]=num[j];  wrd[i]=wrd[j];
+                num[j]=tmp1;    wrd[j]=tmp2;
+            }
+        }
+    }
+}
+
+
+char lowerString(char ch)
+{
+
+
+      if ( ch >= 'A' && ch <= 'Z' )
+      {
+         ch = ch + 32;
+      }
+      return ch;
+   }
+
+
+
+
+int StringLength(string str)
+{
+    int i;
+    for ( i = 0; str[i] ; i++);
+
+        return i;
+
+}
+
+
+string cleanupNumbers(string  str,int len)
+{
+	string numbers = "0123456789";
+	for(int i=0; i<len; i++)
+	{
+		for(int j=0;j<numbers.length();j++)
+		{
+			if(str[i]==numbers[j]) {
+                    str[i] = ' ';
+                    break;
+			}
+		}
+	}
+
+	return str;
+}
+
+
+string cleanupPuncuation(string  str,int len)
+{
+	string puncuation = ",./?'\"':;{}[]\\*()%#@!&+-";
+	for(int i=0; i<len; i++)
+	{
+		for(int j=0;j<puncuation.length();j++)
+		{
+			if(str[i]==puncuation[j]) {
+                    str[i] = ' ';
+                    break;
+			}
+		}
+	}
+	return str;
+}
+
+int main()
+{
+    ifstream iFile;
+
+    ofstream oFile;
+
+    iFile.open("soup.txt");
+    string str;
+
+    if(iFile.is_open())
+    {
+
+       int k=0,x=0 ;
+
+        while(getline(iFile,str))
+        {
+
+            len = StringLength(str);
+
+            str = cleanupPuncuation(str,len);
+
+            str = cleanupNumbers(str,len);
+
+    for(int i=0;i< len;i++){
+
+            str[i] = lowerString(str[i]);
+        }
+            cout<<str<<endl;
+
+            string s ;
+            word[k] = str;
+
+            k++;
+
+        }
+            iFile.close();
+            for(int j =0 ; j < k ; j++)
+                {
+                cout << word[j] <<endl;
+                }
+            ifstream iff;
+
+            oFile.open("sp.txt");
+
+            string nw,stw;
+
+            for(int j =0 ; j < k ; j++)
+                {
+
+                stringstream ss(word[j]);
+
+                while(ss>>nw)
+                    {
+
+                        bool found=false;
+
+                        iff.open("stopWord.txt");
+
+                    if(iff.is_open())
+                        {
+
+                        while(iff>>stw)
+                            {
+
+                            if(stw==nw)
+                                {
+
+                                found=true; break;
+                                }
+                            }
+                        }
+                    iff.close();
+
+                    if(found==false)
+                        {
+                        oFile<<nw<<" ";
+                        wrd[x]=nw;
+                        x++;
+                        }
+                    }
+
+                cout<<endl;
+
+                }
+
+            ifstream fl;
+            fl.open("sp.txt");
+            string st;
+            //int cc=0;
+
+            cout<<"\n\nWORD\t\tNUMBER OF PRESENCE\n\n";
+
+            for(int i=0; i<x; i++)
+                {
+                   int cc=1;
+                   if(wrd[i]!="-1")
+                   {
+                        for(int k=i+1; k<x; k++)
+                        {
+                            if(wrd[i]==wrd[k])
+                            {
+                                wrd[k]="-1";
+                                cc++;  //break;
+                            }
+                        }
+                   }
+
+                if(wrd[i]!="-1")
+                    {
+                    num[i]=cc;
+
+                    //cout<<wrd[i]<<"\t\t"<<cc<<endl;
+                    }
+                }
+
+            Sort(wrd,num,x);
+            for(int i=0; i<x; i++)
+                {
+               if(wrd[i]!="-1") cout<<wrd[i]<<"\t\t"<<num[i]<<endl;
+                }
+
+    }
+
+}
+
