bf52d451d73c4f030a2ea9baf207a2842646c552
Taslima12345 <35329197+Taslima12345@users.noreply.github.com>
2018-05-27 23:48:11 -0700

    Add files via upload

--- spl1111.cpp
+++ spl1111.cpp
@@ -6,79 +6,115 @@
 using namespace  std ;
 char **arr;
 string str;
-string word[10000],wrd[10000];
+string line[10000],word[10000];
 int num[10000];
 int len=0;
 
-
-void Sort(string *wrd,int *num ,int x)
+void Now_Merge( int left, int middle, int right)
 {
-    int tmp1;   string tmp2;
-    for(int i=0; i<x-1; i++)
+    int i, j, k;
+    int n1 = middle - left + 1;
+    int n2 =  right - middle;
+    string temp;
+
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++)
+        L[i] = num[left + i];
+
+    for (j = 0; j < n2; j++)
+        R[j] = num[ (middle +1) + j];
+
+    i = 0;
+    j = 0;
+    k = left;
+
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
         {
-        for(int j=i+1; j<x; j++)
-        {
-            if(num[j]>num[i])
-            {
-                tmp1= num[i];   tmp2=wrd[i];
-                num[i]=num[j];  wrd[i]=wrd[j];
-                num[j]=tmp1;    wrd[j]=tmp2;
-            }
+            num[k] = L[i];
+
+            temp = word[k];
+            word[k] = word[i];
+            word[i] = temp;
+
+            i++;
         }
+        else
+        {
+            num[k] = R[j];
+
+            temp = word[k];
+            word[k] = word[j];
+            word[j] = temp;
+
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1)
+    {
+        num[k] = L[i];
+
+        temp = word[k];
+        word[k] = word[i];
+        word[i] = temp;
+
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        num[k] = R[j];
+
+        temp = word[k];
+        word[k] = word[i];
+        word[i] = temp;
+
+        j++;
+        k++;
     }
 }
 
 
-char lowerString(char ch)
+void Do_MergeSort( int left, int right)
 {
+    if (left < right)
+    {
+        int middle = left+(right-left)/2;
 
+        Do_MergeSort( left, middle);
+        Do_MergeSort( middle+1, right);
 
-      if ( ch >= 'A' && ch <= 'Z' )
-      {
-         ch = ch + 32;
-      }
-      return ch;
-   }
-
-
-
+        Now_Merge( left, middle, right);
+    }
+}
 
 int StringLength(string str)
 {
     int i;
     for ( i = 0; str[i] ; i++);
 
-        return i;
+    return i;
 
 }
 
+string PunctuationCleaningAndLowercasing(string  str,int len)
 
-string cleanupNumbers(string  str,int len)
 {
-	string numbers = "0123456789";
+	string habijabi = ",./?'\"':;{}[]\\*()%#@!&+-";
 	for(int i=0; i<len; i++)
 	{
-		for(int j=0;j<numbers.length();j++)
+        if ( str[i] >= 'A' && str[i] <= 'Z' ){
+            str[i] = str[i] + 32;
+            continue;
+        }
+		for(int j=0;j<habijabi.length();j++)
 		{
-			if(str[i]==numbers[j]) {
-                    str[i] = ' ';
-                    break;
-			}
-		}
-	}
-
-	return str;
-}
-
-
-string cleanupPuncuation(string  str,int len)
-{
-	string puncuation = ",./?'\"':;{}[]\\*()%#@!&+-";
-	for(int i=0; i<len; i++)
-	{
-		for(int j=0;j<puncuation.length();j++)
-		{
-			if(str[i]==puncuation[j]) {
+			if(str[i]==habijabi[j]) {
                     str[i] = ' ';
                     break;
 			}
@@ -93,122 +129,99 @@ int main()
 
     ofstream oFile;
 
-    iFile.open("soup.txt");
+    iFile.open("TextToRead1.txt");
     string str;
 
     if(iFile.is_open())
     {
 
-       int k=0,x=0 ;
+        int LineNo=0 ;
 
         while(getline(iFile,str))
         {
 
             len = StringLength(str);
 
-            str = cleanupPuncuation(str,len);
-
-            str = cleanupNumbers(str,len);
-
-    for(int i=0;i< len;i++){
-
-            str[i] = lowerString(str[i]);
-        }
-            cout<<str<<endl;
+            str = PunctuationCleaningAndLowercasing(str,len) ;
 
             string s ;
-            word[k] = str;
+            line[LineNo] = str;
 
-            k++;
+            LineNo++;
 
         }
-            iFile.close();
-            for(int j =0 ; j < k ; j++)
-                {
-                cout << word[j] <<endl;
-                }
-            ifstream iff;
+        iFile.close();
 
-            oFile.open("sp.txt");
+        ifstream iff;
 
-            string nw,stw;
+        oFile.open("pureWords.txt");
 
-            for(int j =0 ; j < k ; j++)
-                {
+        string WordFromTextLine, StopWord;
+        int TotalWord = 0;
 
-                stringstream ss(word[j]);
+        for(int j =0 ; j < LineNo ; j++){
 
-                while(ss>>nw)
-                    {
+            stringstream ss(line[j]);
 
-                        bool found=false;
+            while(ss>>WordFromTextLine){
 
-                        iff.open("stopWord.txt");
+                bool found=false;
 
-                    if(iff.is_open())
-                        {
+                iff.open("stopWords.txt");
 
-                        while(iff>>stw)
-                            {
+                if(iff.is_open()){
 
-                            if(stw==nw)
-                                {
+                    while(iff>>StopWord){
 
-                                found=true; break;
-                                }
-                            }
-                        }
-                    iff.close();
+                        if(StopWord==WordFromTextLine){
 
-                    if(found==false)
-                        {
-                        oFile<<nw<<" ";
-                        wrd[x]=nw;
-                        x++;
+                            found=true;
+                            break;
                         }
                     }
-
-                cout<<endl;
-
                 }
+                iff.close();
 
-            ifstream fl;
-            fl.open("sp.txt");
-            string st;
-            //int cc=0;
+                if(found==false){
 
-            cout<<"\n\nWORD\t\tNUMBER OF PRESENCE\n\n";
+                    oFile<<WordFromTextLine<<" ";
+                    word[TotalWord]=WordFromTextLine;
+                    TotalWord++;
+                }
+            }
 
-            for(int i=0; i<x; i++)
-                {
-                   int cc=1;
-                   if(wrd[i]!="-1")
-                   {
-                        for(int k=i+1; k<x; k++)
-                        {
-                            if(wrd[i]==wrd[k])
-                            {
-                                wrd[k]="-1";
-                                cc++;  //break;
-                            }
-                        }
-                   }
+        }
+        /*ifstream fl;
+        fl.open("pureWords.txt");
+        string st;
+        */
+        cout<<"\n\nWORD\t\tNUMBER OF PRESENCE\n\n";
 
-                if(wrd[i]!="-1")
-                    {
-                    num[i]=cc;
+        for(int i=0; i<TotalWord; i++){
 
-                    //cout<<wrd[i]<<"\t\t"<<cc<<endl;
+            int WordCount=1;
+            if(word[i]!="-1"){
+
+                for(int k=i+1; k<TotalWord; k++){
+
+                    if(word[i]==word[k]){
+                        word[k]="-1";
+                        WordCount++;
                     }
                 }
+            }
+            if(word[i]!="-1"){
+                num[i]=WordCount;
 
-            Sort(wrd,num,x);
-            for(int i=0; i<x; i++)
-                {
-               if(wrd[i]!="-1") cout<<wrd[i]<<"\t\t"<<num[i]<<endl;
-                }
+            }
+        }
+        Do_MergeSort( 0, TotalWord-1 );
+
+        for(int i=0; i<TotalWord; i++){
+            if(word[i]!="-1")
+                cout<<word[i]<<"\t\t"<<num[i]<<endl;
+        }
 
     }
-
+    return 0;
 }
-
